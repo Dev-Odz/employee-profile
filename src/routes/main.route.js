abb79fefeb23a31ebc7063acd4bb5ea461a4361e
Dev-Odz <99387418+Dev-Odz@users.noreply.github.com>
2026-03-16 22:12:13 +0800

    CI/CD TEST

--- src/routes/main.route.js
+++ src/routes/main.route.js
@@ -3,8 +3,7 @@ const { Router } = require("express");
 const router = Router();
 
 const mainRoute = router.get("/", (req, res) => {
-	res.send("Welcome to the Employee Profile API");
-	console.log(`This is a test that the CI/CD pipeline is working correctly.`);
+	res.send("Welcome to the Employee Profile API, Yey!");
 });
 
 module.exports = router;
